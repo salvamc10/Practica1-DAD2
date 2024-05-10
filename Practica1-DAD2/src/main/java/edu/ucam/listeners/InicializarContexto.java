16743597602121838e877f252fc034dd5e325a59
salvamc10 <127850677+salvamc10@users.noreply.github.com>
2024-05-10 18:27:06 +0200

    Convocatoria
    He actualizado la lógica de la convocatoria

--- Practica1-DAD2/src/main/java/edu/ucam/listeners/InicializarContexto.java
+++ Practica1-DAD2/src/main/java/edu/ucam/listeners/InicializarContexto.java
@@ -5,6 +5,7 @@ import java.util.Hashtable;
 import edu.ucam.pojos.User;
 import edu.ucam.pojos.Asignatura;
 import edu.ucam.pojos.Convocatoria;
+import edu.ucam.pojos.Turno;
 import edu.ucam.servlets.Control;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
@@ -31,20 +32,23 @@ public class InicializarContexto implements ServletContextListener {
         System.out.println("Inicializando el contexto....");
         
         // Creamos las Hashtable para cada CRUD
-        Hashtable<String, User> users = new Hashtable<String, User>();       
-        Hashtable<String, Asignatura> asignaturas = new Hashtable<String, Asignatura>();   
+        Hashtable<String, User> users = new Hashtable<String, User>(); 
         Hashtable<String, Convocatoria> convocatorias = new Hashtable<String, Convocatoria>();
+        Hashtable<String, Asignatura> asignaturas = new Hashtable<String, Asignatura>();   
+        Hashtable<String, Turno> turnos = new Hashtable<String, Turno>();
         
         // Añadirmos 2 usuarios, 1 asignatura, 1 convocatoria
         users.put("admin", new User("admin", "admin"));    
         users.put("salva", new User("salva", "salva"));
-        asignaturas.put("1", new Asignatura("1", "Matematicas"));
         convocatorias.put("1", new Convocatoria("1", "Ordinaria"));
+        asignaturas.put("1", new Asignatura("1", "Matematicas"));
+        turnos.put("1", new Turno("1", "Mañana", asignaturas));
         
         // Asignar las Hashtable al contexto del servlet
         sce.getServletContext().setAttribute("users", users);
         sce.getServletContext().setAttribute("asignaturas", asignaturas);
         sce.getServletContext().setAttribute("convocatorias", convocatorias);
+        sce.getServletContext().setAttribute("turnos", turnos);
     	
     }
 
--- Practica1-DAD2/src/main/webapp/WEB-INF/jsp/editarConvocatoria.jsp
+++ Practica1-DAD2/src/main/webapp/WEB-INF/jsp/editarConvocatoria.jsp
@@ -70,13 +70,13 @@
 <body>
 
 <div class="container">
-    <h2>Cambiar nombre</h2>
+    <h2>Cambiar nombre de la convocatoria</h2>
     
     <form action="Control" method="post">
         <input type="hidden" name="idaccion" value="EditarConvocatoria">
-        <input type="hidden" name="id" value="<%= request.getParameter("id") %>">
-        <label for="nombre">Nuevo Nombre:</label>
-        <input type="text" id="nombre" name="nombre" required>
+        <input type="hidden" name="idConvocatoria" value="<%= request.getParameter("idConvocatoria") %>">
+        <label for="nombreConvocatoria">Nuevo nombre de la convocatoria:</label>
+        <input type="text" id="nombreConvocatoria" name="nombreConvocatoria" required>
         <input type="submit" value="Cambiar nombre">
     </form>
 </div>
