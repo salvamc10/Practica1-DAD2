7321ca8721b57a0ad2d0268f07c35c0551182537
salvamc10 <127850677+salvamc10@users.noreply.github.com>
2024-05-10 11:52:50 +0200

    Convocatorias
    Convocatorias con la clase y contexto inicializado

--- Practica1-DAD2/src/main/java/edu/ucam/listeners/InicializarContexto.java
+++ Practica1-DAD2/src/main/java/edu/ucam/listeners/InicializarContexto.java
@@ -4,6 +4,7 @@ import java.util.Hashtable;
 
 import edu.ucam.pojos.User;
 import edu.ucam.pojos.Asignatura;
+import edu.ucam.pojos.Convocatoria;
 import edu.ucam.servlets.Control;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
@@ -29,26 +30,21 @@ public class InicializarContexto implements ServletContextListener {
     	// Imprimir mensaje de inicialización del contexto
         System.out.println("Inicializando el contexto....");
         
-        // Crear una Hashtable para almacenar usuarios (clave: nombre de usuario, valor: objeto User)
-        Hashtable<String, User> users = new Hashtable<String, User>();
+        // Creamos las Hashtable para cada CRUD
+        Hashtable<String, User> users = new Hashtable<String, User>();       
+        Hashtable<String, Asignatura> asignaturas = new Hashtable<String, Asignatura>();   
+        Hashtable<String, Convocatoria> convocatorias = new Hashtable<String, Convocatoria>();
         
-        // Crear una Hashtable para almacenar asignaturas
-        Hashtable<String, Asignatura> asignaturas = new Hashtable<String, Asignatura>();
-        
-        // Agregar un usuario de ejemplo ("admin" con contraseña "admin") a la Hashtable
-        users.put("admin", new User("admin", "admin"));
-        
-        // Agregar un usuario de ejemplo ("salva" con contraseña "salva") a la Hashtable
+        // Añadirmos 2 usuarios, 1 asignatura, 1 convocatoria
+        users.put("admin", new User("admin", "admin"));    
         users.put("salva", new User("salva", "salva"));
-        
-        // Agregar una asignatura de ejemplo ("1" con nombre "matemáticas") a la Hashtable
         asignaturas.put("1", new Asignatura("1", "Matematicas"));
+        convocatorias.put("1", new Convocatoria("1", "Matematicas", "Mañana"));
         
-        // Asignar la Hashtable de usuarios al contexto del servlet con el nombre "users"
+        // Asignar las Hashtable al contexto del servlet
         sce.getServletContext().setAttribute("users", users);
-        
-        //Asignar la Hashtable de asignaturas al contexto del servlet con el nombre "asignaturas"
         sce.getServletContext().setAttribute("asignaturas", asignaturas);
+        sce.getServletContext().setAttribute("convocatorias", convocatorias);
     	
     }
 
--- Practica1-DAD2/src/main/java/edu/ucam/pojos/Convocatoria.java
+++ Practica1-DAD2/src/main/java/edu/ucam/pojos/Convocatoria.java
@@ -2,4 +2,38 @@ package edu.ucam.pojos;
 
 public class Convocatoria {
 
+	private String id;
+	private String asignatura;
+	private String turno;
+	
+	public Convocatoria(String id, String asignatura, String turno) {
+		this.id = id;
+		this.asignatura = asignatura;
+		this.turno = turno;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getAsignatura() {
+		return asignatura;
+	}
+
+	public void setAsignatura(String asignatura) {
+		this.asignatura = asignatura;
+	}
+
+	public String getTurno() {
+		return turno;
+	}
+
+	public void setTurno(String turno) {
+		this.turno = turno;
+	}
+
 }
