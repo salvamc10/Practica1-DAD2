0b9dcd53d21c50d9331e5d4b8aec02a317dabc54
salvamc10 <127850677+salvamc10@users.noreply.github.com>
2024-05-10 17:00:26 +0200

    Creación de la clase Turno
    He creado al clase turno la cual tiene un atributo Hashtable para las asignaturas

---
+++ Practica1-DAD2/src/main/java/edu/ucam/pojos/Turno.java
@@ -0,0 +1,42 @@
+package edu.ucam.pojos;
+
+import java.util.Hashtable;
+
+public class Turno {
+	
+	private String id;
+	private String nombre;
+	private Hashtable<String, Asignatura> asignaturas;
+	
+	public Turno(String id, String nombre, Hashtable<String, Asignatura> asignaturas) {
+		this.id = id;
+		this.nombre = nombre;
+		this.asignaturas = asignaturas;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Hashtable<String, Asignatura> getAsignaturas() {
+		return asignaturas;
+	}
+
+	public void setAsignaturas(Hashtable<String, Asignatura> asignaturas) {
+		this.asignaturas = asignaturas;
+	}
+	
+
+}
